660243f7164b543186541059dbbd5de29de2b1d0
Christian <chsanleo@gmail.com>
2020-08-25 13:16:44 +0200

    create validations for login

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -6,17 +6,36 @@ export const validations = {
 };
 
 function validateIdentification(identification) {
-    if (utils.isNullOrEmpty(identification.email)) {
-        return "Email must be provided.";
-    }
+    return validateEmail(identification.email)
 };
 
-function validateContactUs(contactUs){
+function validateEmail(email) {
+    if (!utils.isNullOrEmpty(email)) {
+        if (!email.includes('@') || !email.includes('.')) {
+            return ' Email must have a correct format. ';
+        }
+    } else {
+        return ' Email must be provided. ';
+    }
+    return '';
+}
+function validateLogin(credentials) {
+
+    let error = validateEmail(credentials.email);
+
+    if (utils.isNullOrEmpty(credentials.password)) {
+        error += ' Password must be provided. ';
+    }
+
+    return error;
+};
+
+function validateContactUs(contactUs) {
     let error = utils.EMPTY;
 
-    if(utils.isNullOrEmpty(contactUs.message)){ error += ' Message body must be provided. '; }
-    if(utils.isNullOrEmpty(contactUs.subject)){ error += ' Subject must be provided. '; }
-    if(utils.isNullOrEmpty(contactUs.email)){ error += ' Email must be provided. '; }
+    if (utils.isNullOrEmpty(contactUs.message)) { error += ' Message body must be provided. '; }
+    if (utils.isNullOrEmpty(contactUs.subject)) { error += ' Subject must be provided. '; }
+    if (utils.isNullOrEmpty(contactUs.email)) { error += ' Email must be provided. '; }
 
-    if(!utils.isNullOrEmpty(error)){throw Error(error);}
+    if (!utils.isNullOrEmpty(error)) { throw Error(error); }
 };
