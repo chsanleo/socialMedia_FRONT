2680461f3329ee8f458fe2c3588086e0eecd6981
Christian <chsanleo@gmail.com>
2020-08-25 13:05:36 +0200

    LogOut view

---
+++ src/views/logout/logout.jsx
@@ -0,0 +1,31 @@
+import React from '../../../node_modules/react';
+import { Redirect } from '../../../node_modules/react-router-dom';
+
+import { userService } from '../../services/userService.js';
+
+class LogOut extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            redirect: false
+        }
+    }
+    componentDidMount() {
+        userService.logout();
+        setTimeout(() => {
+            this.setState({ redirect: true });
+        }, 2500);
+    }
+    render() {
+        if (this.state.redirect) {
+            return (
+                <Redirect to={'/'} />
+            )
+        }
+        return (
+            <h3 align="center">Thanks for use our application!! We gonna miss you!</h3>
+        );
+    }
+}
+export default LogOut;
