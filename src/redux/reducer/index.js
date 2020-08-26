6b1513d1f2854e5431b2a1d6fb7af72b7bc89c4c
Christian <chsanleo@gmail.com>
2020-08-26 02:53:25 +0200

    add event reducer to index

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 
 import usersReducer from './user.js';
+import eventReducer from './event.js';
 
 const reducer = combineReducers({
-    users: usersReducer
+    users: usersReducer,
+    event:eventReducer
 });
 export default reducer;
