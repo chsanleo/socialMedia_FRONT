f87c9fe58b7698b1c95442a3b362fef509f99e49
Christian <chsanleo@gmail.com>
2020-08-27 17:35:02 +0200

    add evaluation axios token

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import API_URL from './api';
+import { utils } from '../utils/utils';
 
 axios.defaults.baseURL = API_URL;
 
-axios.defaults.headers.Authorization = localStorage.getItem('authToken');
+let token = localStorage.getItem('authToken');
+if(!utils.isNullOrEmpty(token)){  axios.defaults.headers.authorization = token; }
+
