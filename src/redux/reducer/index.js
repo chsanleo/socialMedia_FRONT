cbc3d1b57f1f43886faa6083caf5ae2fbe4ffb2c
Christian <chsanleo@gmail.com>
2020-08-27 02:23:41 +0200

    add event and data to index redux

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 
 import usersReducer from './user.js';
 import eventReducer from './event.js';
+import datareducer from './data.js';
 
 const reducer = combineReducers({
     users: usersReducer,
-    event:eventReducer
+    event: eventReducer,
+    data: datareducer
 });
 export default reducer;
