7786ea244e34c25c124228ebc0d211a500bc0c5c
Christian <chsanleo@gmail.com>
2020-08-29 16:36:37 +0200

    add new format & add remote pic

--- src/views/profile/profile.jsx
+++ src/views/profile/profile.jsx
@@ -6,7 +6,7 @@ import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 
 import CountryList from '../../components/countryList/countryList.jsx';
-import TypesList  from '../../components/typesList/typesList.jsx';
+import TypesList from '../../components/typesList/typesList.jsx';
 import { userService } from '../../services/userService.js';
 import { validations } from '../../utils/validations.js';
 import { utils } from '../../utils/utils.js';
@@ -39,7 +39,7 @@ class Profile extends React.Component {
             name: this.props.user.name,
             surname: this.props.user.surname,
             birthdate: this.props.user.birthdate,
-            pic_path: this.props.user.pic_path != null ? this.props.user.pic_path : './defaultProfile.png',
+            pic_path: this.props.user.pic_path,
             address: this.props.user.address,
             email: this.props.user.email,
             city: this.props.user.city,
@@ -56,6 +56,22 @@ class Profile extends React.Component {
     }
     setDate(date) {
         this.setState({ birthdate: date });
+    }
+    isIMG() {
+        if(utils.isNullOrEmpty(this.state.pic_path)){
+            return (<img src='defaultProfile.png' width="200px" alt="eventPhoto" />);
+        }
+        let url = this.state.pic_path.split('.');
+        switch (url[url.length - 1]) {
+            case 'jpg':
+            case 'JPG':
+            case 'jpeg':
+            case 'JPEG':
+            case 'png':
+            case 'PNG': return (<img src={this.state.pic_path} width="200px" alt="eventPhoto" />);
+
+            default: return (<img src='defaultProfile.png' width="200px" alt="eventPhoto" />);
+        }
     }
     pressUpdate = (ev) => {
         ev.preventDefault();
@@ -91,41 +107,52 @@ class Profile extends React.Component {
     render() {
         return (
             <div className="centerInfo">
+
                 <form onSubmit={this.pressUpdate}>
                     <h2>Personal information</h2>
-                    <img src={this.state.pic_path} alt="userPhoto" /><br />
-                    <span className="errorText">{this.state.msgError}</span>
-                    <p><label>Username</label> &nbsp;
+                    <div className="formsFormat">
+                        <div className="image">
+                            {this.isIMG()}
+                            <p><label>Add remote Photo (url):</label><br />
+                                <input type="text" name="pic_path" value={this.state.pic_path || ''}
+                                    onChange={this.handleChange}></input><br /></p>
+                            <br />
+                        </div>
+                        <div>
+                        <span className="errorText">{this.state.msgError}</span>
+                            <p><label>Username</label> &nbsp;
                     <input type="text" name="username" value={this.state.username || ''}
-                        onChange={this.handleChange}></input><br /></p>
-                    <p><label>Name</label>&nbsp;
+                                    onChange={this.handleChange}></input><br /></p>
+                            <p><label>Name</label>&nbsp;
                     <input className="inputs" type="text" name="name" value={this.state.name || ''}
-                        onChange={this.handleChange}></input><br /></p>
-                    <p><label>Surname</label>&nbsp;
+                                    onChange={this.handleChange}></input><br /></p>
+                            <p><label>Surname</label>&nbsp;
                     <input type="text" name="surname" value={this.state.surname || ''}
-                        onChange={this.handleChange}></input><br /></p>
-                         <p><label>Bith date</label>&nbsp;
+                                    onChange={this.handleChange}></input><br /></p>
+                            <label>Bith date</label>&nbsp;
                          <DatePicker
-                            selected={this.state.birthdate}
-                            onChange={date => this.setDate(date)}
-                            maxDate={new Date()}
-                            name="date"
-                            dateFormat="dd/MM/yyyy"
-                        /></p>
-                    <p><label>Address</label>&nbsp;
+                                    selected={this.state.birthdate}
+                                    onChange={date => this.setDate(date)}
+                                    maxDate={new Date()}
+                                    name="date"
+                                    dateFormat="dd/MM/yyyy"
+                                /><p></p>
+                            <p><label>Address</label>&nbsp;
                     <input type="text" name="address" value={this.state.address || ''}
-                        onChange={this.handleChange}></input><br /></p>
-                    <p><label>City</label>&nbsp;
+                                    onChange={this.handleChange}></input><br /></p>
+                            <p><label>City</label>&nbsp;
                     <input type="text" name="city" value={this.state.city || ''}
-                        onChange={this.handleChange}></input><br /></p>
-                        <p><label>Job</label>&nbsp;
+                                    onChange={this.handleChange}></input><br /></p>
+                            <p><label>Job</label>&nbsp;
                     <input type="text" name="job" value={this.state.job || ''}
-                        onChange={this.handleChange}></input><br /></p>
-                    <p><label>Hobbies: </label> {this.props.user.hobbies};<br />
-                    <TypesList setHobby={this.setHobby} readOnly /></p>
-                    <p><label>Country: </label> {this.props.user.country}<br />
-                    <CountryList setCountry={this.setCountry} readOnly /></p>
-                    <br />
+                                    onChange={this.handleChange}></input><br /></p>
+                            <p><label>Hobbies: </label> {this.props.user.hobbies};<br />
+                                <TypesList setHobby={this.setHobby} readOnly /></p>
+                            <p><label>Country: </label> {this.props.user.country}<br />
+                                <CountryList setCountry={this.setCountry} readOnly /></p>
+                            <br />
+                        </div>
+                    </div>
                     <button type="submit">Update profile!</button><br />
                     <NavLink to="/forgotPass">Change my password</NavLink>
                 </form>
