365ec17491049b69b40b50a4b45936c5d2f82d6c
Christian <chsanleo@gmail.com>
2020-08-30 16:48:46 +0200

    create conection back with like, join and dislike

--- src/repositories/eventRepository.js
+++ src/repositories/eventRepository.js
@@ -2,17 +2,17 @@ import axios from 'axios';
 import { utils } from '../utils/utils';
 
 export const eventRepository = {
-    getAllEvents, createEvent
+    getAllEvents, createEvent, joinEvent, likeEvent, dislikeEvent
 };
 
 async function getAllEvents(type) {
-    let token  = localStorage.getItem('authToken');
-    if(!utils.isNullOrEmpty(token)){ axios.defaults.headers.authorization = token }
-    else{token ="no hay";}
+    let token = localStorage.getItem('authToken');
+    if (!utils.isNullOrEmpty(token)) { axios.defaults.headers.authorization = token }
+    else { token = "no hay"; }
 
     //console.log(token);
     try {
-        const res = await axios.post(`event/events`,type/*, {
+        const res = await axios.post(`event/events`, type/*, {
             headers: { 'authorization': token }
         }*/);
         return res.data;
@@ -26,6 +26,36 @@ async function getAllEvents(type) {
 async function createEvent(event) {
     try {
         const res = await axios.post(`event/create`, event);
+        return res.data;
+    }
+    catch (error) {
+        console.log(error)
+        throw Error("Could not create event.");
+    }
+};
+async function joinEvent(userIntoEvent) {
+    try {
+        const res = await axios.post(`event/joinEvent`, userIntoEvent);
+        return res.data;
+    }
+    catch (error) {
+        console.log(error)
+        throw Error("Could not create event.");
+    }
+};
+async function likeEvent(userIntoEvent) {
+    try {
+        const res = await axios.post(`event/likeEvent`, userIntoEvent);
+        return res.data;
+    }
+    catch (error) {
+        console.log(error)
+        throw Error("Could not create event.");
+    }
+};
+async function dislikeEvent(userIntoEvent) {
+    try {
+        const res = await axios.post(`event/unlikeEvent`, userIntoEvent);
         return res.data;
     }
     catch (error) {
