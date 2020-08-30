db8178e834709f10112f51f440af975bd9026645
Christian <chsanleo@gmail.com>
2020-08-30 16:45:52 +0200

    Show default image and return a event when click

--- src/components/eventItem/eventItem.jsx
+++ src/components/eventItem/eventItem.jsx
@@ -18,9 +18,11 @@ class EventItem extends React.Component {
         return (
             <div className="eventItem" >
                 {this.props.eventList?.map(event => (
-                    <div key={event._id} >
+                    <div key={event._id}>
                         <h3>{event.title}</h3><p>{this.cleanDate(event.date)}</p>
-                        <img src={event.pic_path != null ? event.pic_path : 'Activities.png'} />
+                        <img src={event.pic_path !== '' ? event.pic_path : 'Activities.png'}
+                            onClick={e => this.props.setEvent( event )}
+                            alt ="eventPic" />
                         <p className="createBy">Create by: {event.owner[0].username}</p>
                     </div>
                 ))}
