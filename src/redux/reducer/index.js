da9b556b4ffb20d8e526e164c268053c1be94e7c
Christian <chsanleo@gmail.com>
2020-08-31 20:23:48 +0200

    add to index messageReducer

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -2,11 +2,11 @@ import { combineReducers } from 'redux';
 
 import usersReducer from './user.js';
 import eventReducer from './event.js';
-import datareducer from './data.js';
+import dataReducer from './data.js';
+import messageReducer from './messages';
 
 const reducer = combineReducers({
-    users: usersReducer,
-    event: eventReducer,
-    data: datareducer
+    users: usersReducer, event: eventReducer,
+    data: dataReducer, message: messageReducer
 });
 export default reducer;
