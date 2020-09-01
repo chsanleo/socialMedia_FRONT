a3730d5d62e1f2e69b780931ec117c159d44f3ec
Christian <chsanleo@gmail.com>
2020-09-01 02:15:42 +0200

    created method to messages service

--- src/services/messageService.js
+++ src/services/messageService.js
@@ -2,11 +2,32 @@ import { messageRepository } from '../repositories/messageRepository.js';
 import { addMessageListAction } from '../redux/actions/messages.js';
 
 export const messageService = {
-    getAllMessages
+    getAllMessages, createMessage,
+    likeMessage, dislikeMessage, deleteMessage
 };
 
 function getAllMessages(eventMessage) {
     messageRepository.getAllMessages(eventMessage)
         .then(res => addMessageListAction(res))
+        .catch(error => console.log(error));
+};
+function createMessage(message){
+    messageRepository.createMessage(message)
+        .then()
+        .catch(error => console.log(error));
+};
+function likeMessage(message){
+    messageRepository.likeMessage(message)
+        .then( )
+        .catch(error => console.log(error));
+};
+function dislikeMessage(message){
+    messageRepository.dislikeMessage(message)
+        .then()
+        .catch(error => console.log(error));
+};
+function deleteMessage(message){
+    messageRepository.deleteMessage(message)
+        .then()
         .catch(error => console.log(error));
 };
