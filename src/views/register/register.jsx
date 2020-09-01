33d24245da0361a7ea0c1b60a97269c220ad8de9
Christian <chsanleo@gmail.com>
2020-09-01 15:25:46 +0200

    clean code

--- src/views/register/register.jsx
+++ src/views/register/register.jsx
@@ -44,7 +44,7 @@ class Register extends React.Component {
                     <h3>Do you want join us?</h3>
                     <span className="errorText">{this.state.msgError}</span>
                     <input type="text" placeholder="Email" name="email"
-                        value={this.state.email} onChange={this.handleChange}></input>
+                        value={this.state.email} onChange={this.handleChange}/>
                     <button type="submit">Ask for ur Access!</button>
                 </form><br /><br />
                 <img src="./fondo.png" alt="fondo" /><br /><br />
