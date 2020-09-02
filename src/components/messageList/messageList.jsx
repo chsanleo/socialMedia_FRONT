63582cbc52277b3bed5b1223063ed1430c9d702f
Christian <chsanleo@gmail.com>
2020-09-02 19:58:52 +0200

    adapt to new createMessage
    parse, change icon delete,

--- src/components/messageList/messageList.jsx
+++ src/components/messageList/messageList.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Parser from 'html-react-parser';
 
 import './messageList.scss';
 
-import CreateMessage from '../createMessage/createMessage.jsx';
 import { messageService } from '../../services/messageService';
 
 class MesssageList extends React.Component {
@@ -48,35 +48,33 @@ class MesssageList extends React.Component {
             <div>
                 {this.props.messageList?.map(message => (
                     <div className="messageEvent" key={message._id}>
-                        {
-                            message.owner[0].id === this.props.user.id ?
-                                <img src="./delete.png" onClick={e => this.deleteMessage(message._id)}
-                                    width="20px" alt="deletePic" />
-                                : ''
-                        }
+                        
                         <img className="profilePicList"
                             src={message.owner[0].pic_path !== '' ? message.owner[0].pic_path : './defaultProfile.png'}
                             onClick={e => this.props.profile(message.owner[0])}
                             alt="profilePic"
                             title={message.owner[0].username} />
                         &nbsp;&nbsp;{this.cleanDate(message.createdAt)}&nbsp;by&nbsp;{message.owner[0].username}
-                        {this.haveDelete}
+                        {
+                            message.owner[0].id === this.props.user.id ?
+                                <img className="optionsMssg" src="./deleteMssg.png" onClick={e => this.deleteMessage(message._id)}
+                                    alt="deletePic" />
+                                : ''
+                        }
                         <div>
-                            {message.body}<br />
-                            {message.likes.length}
+                            {Parser(message.body)}<br />
+                            {message.likes.length}&nbsp;&nbsp;&nbsp;
                             {
                                 message.likes.length !== 0 &&
                                     message.likes.find(element => element.id === this.props.user.id) ?
-                                    <img className="linkImg" src="./dislike.png" onClick={e => this.dislike(message._id)} alt="dislikePic" />
+                                    <img className="linkImgMssg" src="./dislike.png" onClick={e => this.dislike(message._id)} alt="dislikePic" />
                                     :
-                                    <img className="linkImg" src="./like.png" onClick={e => this.addLike(message._id)} alt="likePic" />
+                                    <img className="linkImgMssg" src="./like.png" onClick={e => this.addLike(message._id)} alt="likePic" />
                             }
                         </div>
                     </div>
                 ))}
-                <CreateMessage />
             </div>
-
         )
     }
 }
