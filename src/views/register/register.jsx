0094118fbeace1d9116e9948132a6b6b848fa9d4
Christian <chsanleo@gmail.com>
2020-09-03 02:39:42 +0200

    add class to enroll button

--- src/views/register/register.jsx
+++ src/views/register/register.jsx
@@ -45,8 +45,9 @@ class Register extends React.Component {
                     <span className="errorText">{this.state.msgError}</span>
                     <input type="text" placeholder="Email" name="email"
                         value={this.state.email} onChange={this.handleChange}/>
-                    <button type="submit">Ask for ur Access!</button>
+                    <button className="buttonAccess" type="submit">Ask for ur Access!</button>
                 </form><br /><br />
+                <Link exact to="/">Return</Link><br /><br />
                 <img src="./fondo.png" alt="fondo" /><br /><br />
                 <Link to="/contactMail"><img src="./contactUs.png" width="50px" alt="Contact Us" /></Link><br />
             </div>
